b9d88da67a22474d6c5a50d27b1cba924f34d0e0
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-06-10 11:45:37 +0200

    Video information extracted for mp4, hls and hlsLive

--- src/js/EpisodeConversor.js
+++ src/js/EpisodeConversor.js
@@ -1,5 +1,94 @@
 import { Data } from "paella-core";
 
+const g_contentTypes = {
+    "presentation/delivery": "presentation",
+    "presenter/delivery": "presenter"
+};
+
+const g_streamTypes = [
+    {
+        enabled: true,
+        streamType: 'mp4',
+        conditions: {
+            mimetype: "video/mp4"
+        },
+        getSourceData: (track) => {
+            const src = track.url;
+            const mimetype = track.mimetype;
+            const resolution = track.video?.resolution || "1x1";
+            const resData = /(\d+)x(\d+)/.exec(resolution);
+            const res = {
+                w: 0,
+                h: 0
+            }
+
+            if (resData) {
+                res.w = resData[1];
+                res.h = resData[2];
+            }
+
+            return { src, mimetype, res }
+        }
+    },
+    {
+        enabled: true,
+        streamType: "hls",
+        conditions: {
+            mimetype: "application/x-mpegURL",
+            live: false
+        },
+        getSourceData: (track) => {
+            const src = track.url;
+            const mimetype = track.mimetype;
+            return { src, mimetype }
+        }
+    },
+    {
+        enabled: true,
+        streamType: "hlsLive",
+        conditions: {
+            mimetype: "application/x-mpegURL",
+            live: true
+        },
+        getSourceData: (track) => {
+            const src = track.url;
+            const mimetype = track.mimetype;
+            return { src, mimetype }
+        }
+    }
+];
+
+function getStreamType(track) {
+    const result = g_streamTypes.find(typeData => {
+        let match = typeData.enabled;
+        for (const condition in typeData.conditions) {
+            if (!match) {
+                break;
+            }
+            const value = typeData.conditions[condition];
+            match = match && track[condition] == value;
+        }
+        return match;
+    })
+    return result;
+}
+
+function getSourceData(track) {
+    let data = null;
+    const type = g_contentTypes[track.type];
+    if (type) {
+        const streamType = getStreamType(track);
+        if (streamType) {
+            data = {
+                source: streamType.getSourceData(track),
+                type: streamType.streamType,
+                content: type
+            }
+        }
+    }
+    return data;
+}
+
 function getMetadata(episode) {
     const { duration, title } = episode.mediapackage;
     
@@ -16,13 +105,17 @@ function getMetadata(episode) {
 function getStreams(episode) {
     const { track } = episode.mediapackage?.media;
 
-    const result = [];
+    const sources = [];
 
     track.forEach(track => {
-        console.log(track);
-    })
+        const sourceData = getSourceData(track);
+        sourceData && sources.push(sourceData);
+    });
 
-    return result;
+    // TODO: group source data
+    console.log(sources);
+
+    return sources;
 }
 
 function getFrameList(episode) {
