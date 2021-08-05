fe9e40c10e71bcacda1dac70a02396777898f00c
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-08-05 13:04:32 +0200

    Transcriptions plugin: list implemented

--- src/plugins/es.upv.paella.transcriptionsPlugin.js
+++ src/plugins/es.upv.paella.transcriptionsPlugin.js
@@ -1,7 +1,8 @@
 import {
     PopUpButtonPlugin,
     createElementWithHtmlText,
-    translate
+    translate,
+    utils
 } from 'paella-core';
 
 import '../css/TranscriptionsPlugin.css'
@@ -10,14 +11,39 @@ import TranscriptionsIcon from '../icons/transcriptions.svg';
 
 export default class TranscriptionsPlugin extends PopUpButtonPlugin {
     async getContent() {
-        const container = createElementWithHtmlText(`
-            <div class="transcriptions-container">Transcriptions</div>
-        `);
+        const { videoContainer } = this.player;
+        const container = createElementWithHtmlText(`<div class="transcriptions-container"></div>`);
+        const searchContainer = createElementWithHtmlText(`
+        <input type="search" placeholder="${translate("Search")}"></input>`, container);
+        searchContainer.addEventListener("click", evt => evt.stopPropagation());
+        const transcriptionsContainer = createElementWithHtmlText(`
+        <ul class="transcriptions-list"></ul>`, container);
+        this.transcriptions.forEach(t => {
+            const id = `transcriptionItem${t.index}`;
+            const transcriptionItem = createElementWithHtmlText(`
+                <li>
+                    <img id="${id}" src="${t.preview}" alt="${t.text}"/>
+                    <span><strong>${utils.secondsToTime(t.time / 1000)}:</strong> ${t.text}</span>
+                </li>`, 
+            transcriptionsContainer);
+            transcriptionItem.addEventListener('click', async evt => {
+                const trimmingOffset = videoContainer.isTrimEnabled ? videoContainer.trimStart : 0;
+                this.player.videoContainer.setCurrentTime(trimmingOffset + t.time / 1000);
+                evt.stopPropagation();
+            });
+        })
         return container;
     }
 
     get popUpType() {
         return "no-modal";
+    }
+
+    async isEnabled() {
+        const enabled = await super.isEnabled();
+        this.transcriptions = this.player.videoManifest.transcriptions;
+        console.log(this.transcriptions);
+        return enabled && this.transcriptions.length > 0;
     }
 
     async load() {
