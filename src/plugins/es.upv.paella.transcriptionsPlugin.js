a5a4ff67c875e35b6eafec32789c6667c7855f57
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-08-06 14:08:55 +0200

    Transcriptions filter implemented

--- src/plugins/es.upv.paella.transcriptionsPlugin.js
+++ src/plugins/es.upv.paella.transcriptionsPlugin.js
@@ -10,28 +10,50 @@ import '../css/TranscriptionsPlugin.css'
 import TranscriptionsIcon from '../icons/transcriptions.svg';
 
 export default class TranscriptionsPlugin extends PopUpButtonPlugin {
-    async getContent() {
+
+    rebuildList(search = "") {
         const { videoContainer } = this.player;
-        const container = createElementWithHtmlText(`<div class="transcriptions-container"></div>`);
-        const searchContainer = createElementWithHtmlText(`
-        <input type="search" placeholder="${translate("Search")}"></input>`, container);
-        searchContainer.addEventListener("click", evt => evt.stopPropagation());
-        const transcriptionsContainer = createElementWithHtmlText(`
-        <ul class="transcriptions-list"></ul>`, container);
+        this._transcriptionsContainer.innerHTML = "";
         this.transcriptions.forEach(t => {
+            if (search !== "") {
+                const searchExp = search.split(" ")
+                    .map(s => `(?:${s})`)
+                    .join("|");
+                const re = new RegExp(searchExp, "i");
+                if (!re.test(t.text)) {
+                    return;
+                }
+            }
             const id = `transcriptionItem${t.index}`;
             const transcriptionItem = createElementWithHtmlText(`
                 <li>
                     <img id="${id}" src="${t.preview}" alt="${t.text}"/>
                     <span><strong>${utils.secondsToTime(t.time / 1000)}:</strong> ${t.text}</span>
                 </li>`, 
-            transcriptionsContainer);
+            this._transcriptionsContainer);
             transcriptionItem.addEventListener('click', async evt => {
                 const trimmingOffset = videoContainer.isTrimEnabled ? videoContainer.trimStart : 0;
                 this.player.videoContainer.setCurrentTime(trimmingOffset + t.time / 1000);
                 evt.stopPropagation();
             });
-        })
+        });
+    }
+
+    async getContent() {
+        
+        const container = createElementWithHtmlText(`<div class="transcriptions-container"></div>`);
+        const searchContainer = createElementWithHtmlText(`
+        <input type="search" placeholder="${translate("Search")}"></input>`, container);
+        searchContainer.addEventListener("click", evt => evt.stopPropagation());
+        searchContainer.addEventListener("keyup", evt => {
+            //console.log(evt.target.value);
+            // TODO: Implement search
+            this.rebuildList(evt.target.value);
+        });
+        const transcriptionsContainer = createElementWithHtmlText(`
+        <ul class="transcriptions-list"></ul>`, container);
+        this._transcriptionsContainer = transcriptionsContainer;
+        this.rebuildList();
         return container;
     }
 
