e398a8dae560ab2f0020aa3dcbeb819f978c6f0a
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-09-22 11:08:56 +0200

    Episodes from series plugin added

---
+++ src/plugins/es.upv.paella.episodesFromSeries.js
@@ -0,0 +1,37 @@
+import { 
+    createElementWithHtmlText,
+    PopUpButtonPlugin,
+    translate
+} from 'paella-core';
+
+export default class EpisodesFromSeriesPlugin extends PopUpButtonPlugin {
+    async getContent() {
+        const {
+            series,
+            seriesTitle
+        } = this.player.videoManifest.metadata;
+
+        const container = createElementWithHtmlText(`
+            <div class="episodes-from-series">
+                <h4>${ translate('Videos in series') } ${ seriesTitle }</h4>
+                <ul>
+                </ul>
+            </div>
+        `);
+
+        const sid = series;
+        const limit = this.config.maxCount || 5;
+        const response = await fetch(`/search/episode.json?sid=${ sid }&limit=${limit}`);
+        if (response.ok) {
+            const responseData = await response.json();
+            const result = responseData["search-results"].result;
+            (Array.isArray(result) ? result : [result]).forEach(({id,dcTitle,mediapackage}) => {
+                const url = `watch.html?id=${id}`;
+                createElementWithHtmlText(`
+                <li><a href="${url}">${dcTitle}</a></li>
+                `,container);
+            });
+        }
+        return container;
+    }
+}
